5527ce7753bf0fb9ca1c1061b04fd981b121fe6c
ShiwaliKarn <shishiwali@gmail.com>
2024-08-16 20:49:39 +0530

    sign up page complete

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,14 +1,19 @@
 import User from "../models/user.model.js";
-import bcryptjs from 'bcryptjs'
+import bcryptjs from "bcryptjs";
 import dotenv from "dotenv";
 
 dotenv.config();
 
-export const signUp = async (req, res) => {
-  const { username, email, password } = req.body;
-  const salt = bcryptjs.genSaltSync(Number(process.env.SALT));
-  const hashedPassword =bcryptjs.hashSync(password,salt);
-  const newUser = new User({ username, email, password: hashedPassword });
-  await newUser.save();
-  res.status(201).json('User created successfully!')
+export const signUp = async (req, res, next) => {
+    const { username, email, password } = req.body;
+    const salt = bcryptjs.genSaltSync(Number(process.env.SALT));
+    const hashedPassword = bcryptjs.hashSync(password, salt);
+    const newUser = new User({ username, email, password: hashedPassword });
+    try {
+        await newUser.save();
+        res.status(201).json("User created successfully!");
+
+    } catch (error) {
+       next(error);
+    }
 };
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
-import SignOut from "./pages/SignOut";
+import SignUp from "./pages/SignUp";
 import SignIn from "./pages/SignIn";
 import About from "./pages/About";
 import Profile from "./pages/Profile";
@@ -13,7 +13,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/sign-in" element={<SignIn />} />
-        <Route path="/sign-out" element={<SignOut />} />
+        <Route path="/sign-up" element={<SignUp />} />
         <Route path="/profile" element={<Profile />} />
         <Route path="/about" element={<About />} />
       </Routes>
